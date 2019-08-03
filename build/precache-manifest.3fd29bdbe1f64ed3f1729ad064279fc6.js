18dd1ccd6f83b5350d57802474d72458d9450790
jason <jason_doan94@yahoo.com>
2019-08-03 10:56:44 -0400

    react

---
+++ build/precache-manifest.3fd29bdbe1f64ed3f1729ad064279fc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e8f4c6f3ee8d8aa24d2b899c968945",
+    "url": "/iamjayy/clicky-game-react/index.html"
+  },
+  {
+    "revision": "2d886d2088aa44f99730",
+    "url": "/iamjayy/clicky-game-react/static/css/main.cd1b3488.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/iamjayy/clicky-game-react/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "2d886d2088aa44f99730",
+    "url": "/iamjayy/clicky-game-react/static/js/main.cd2295e5.chunk.js"
+  },
+  {
+    "revision": "f0e2d279f75043c6c3a1",
+    "url": "/iamjayy/clicky-game-react/static/js/runtime~main.bdf5f94b.js"
+  }
+]);
---
+++ src/components/board/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import Card from '../card'
+import './styles.css'
+
+export default function Board({ 
+    disabled, 
+    dimension, 
+    cards, 
+    flipped,
+    solved,
+    handleClick 
+}) {
+    return (
+        <div className="board">
+            {cards.map((card) => (
+                <Card
+                    key={card.id}
+                    id={card.id}
+                    type={card.type}
+                    width={dimension / 4.5}
+                    height={dimension / 4.5}
+                    flipped={flipped.includes(card.id)}
+                    solved={solved.includes(cards.id)}
+                    handleClick={handleClick}
+                    disabled={disabled || solved.includes(cards.id)}
+                />
+            ))}
+        </div>
+    )
+}
+Board.propTypes = {
+    disabled: PropTypes.bool.isRequired,
+    dimension: PropTypes.number.isRequired,
+    cards: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+    flipped: PropTypes.arrayOf(PropTypes.number).isRequired,
+    solved: PropTypes.arrayOf(PropTypes.number).isRequired,
+    handleCLick: PropTypes.func.isRequired
+}
